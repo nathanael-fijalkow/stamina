73c8acaf442bce58c5df6cfee2054fe5b87fe667
deniskup <denis.kuperberg@gmail.com>
2014-12-18 16:06:41 +0100

    hpp of OneCounter

--- Sandbox/Matrix.hpp
+++ Sandbox/Matrix.hpp
@@ -218,19 +218,19 @@ public:
 	ProbMatrix(const ExplicitMatrix &);
 
 	// Print
-	virtual void print(ostream& os = cout) const;
+	void print(ostream& os = cout) const;
 
 	// Equality operator
-	virtual bool operator == (const ProbMatrix & mat) const;
+	bool operator == (const ProbMatrix & mat) const;
 
 	// Function computing the product and stabilization
 	// They update the matrices, rows and columns
 	//The caller is in charge of deleting the returned object
-	virtual Matrix * prod(const Matrix  *) const;
+	Matrix * prod(const Matrix  *) const;
 
 	// compute stabilisation
 	//The caller is in charge of deleting the returned object
-	virtual Matrix * stab() const;
+	Matrix * stab() const;
 
 	// computes the list of recurrent states.
 	const Vector * recurrent_states() const;
@@ -280,9 +280,54 @@ protected:
 
 class OneCounterMatrix : public Matrix
 {
+
+	//encoding for counter actions, smaller is better
+#define RESET 0
+#define EPS 1
+#define INC 2
+#define OM 3
+
 public:
+	// Constructor from explicit representation
+	OneCounterMatrix(const ExplicitMatrix &);
+
+	// Print
+	void print(ostream& os = cout) const;
+
+	// Equality operator
+	bool operator == (const OneCounterMatrix & mat) const;
+
+	// Function computing the product and stabilization
+	// They update the matrices, rows and columns
+	//The caller is in charge of deleting the returned object
+	Matrix * prod(const Matrix  *) const;
+
+	// compute stabilisation
+	//The caller is in charge of deleting the returned object
+	Matrix * stab() const;
+
+	// Function checking whether a matrix is idempotent
+	bool isIdempotent() const;
 
 protected:
+
+	// Four C-style matrices of size stateNb containing all rows, state per state
+	const Vector ** rows[4];
+	const Vector ** cols[4];
+
+	void update_hash()
+	{
+		_hash = 0;
+		for(int i=0;i<4;i++){
+		for (const Vector ** p = rows[i]; p != rows[i] + stateNb; p++)
+			_hash ^= hash_value((size_t)*p) + 0x9e3779b9 + (_hash << 6) + (_hash >> 2);
+		for (const Vector ** p = cols[i]; p != cols[i] + stateNb; p++)
+			_hash ^= hash_value((size_t)*p) + 0x9e3779b9 + (_hash << 6) + (_hash >> 2);
+		}
+	};
+
+	// Function allocating memory for rows and cols
+	void allocate();
 
 };
 
