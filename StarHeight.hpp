8e184301adb9132f6e6e797604bcad66b0af172c
Denis <denis.kuperberg@gmail.com>
2015-02-05 15:29:22 +0100

    Starheight construction of nested automaton

---
+++ StarHeight.hpp
@@ -0,0 +1,15 @@
+#ifndef STARHEIGHT_HPP
+#define STARHEIGHT_HPP
+
+#include "Automata.hpp"
+
+
+// B-automaton testing if L(aut) has star-height at most k.
+//aut must be either deterministic, or more generally can be the dual (wrt accepting state) of a non-deterministic automaton for the complement.
+MultiCounterAut* toNestedBaut(ClassicAut *aut, char k);
+
+
+#endif
+
+
+
